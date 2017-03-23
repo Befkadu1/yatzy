fcb8fc0be981bdfd0ccea68a32b773558ca8df6f
Joel Nilsson <joel.o.nilsson@gmail.com>
2017-03-23 15:40:07 +0100

    Börjat på scoreboard-klass

---
+++ js/classes/scoreBoard.class.js
@@ -0,0 +1,21 @@
+class ScoreBoard {
+	constructor(){
+		this.ones;
+		this.twos;
+		this.threes;
+		this.fours;
+		this.fives;
+		this.sixes;
+		this.sum;
+		this.bonus;
+		this.onePair;
+		this.twoPairs;
+		this.threeOfAKind;
+		this.fourOfAKind;
+		this.smallStraight;
+		this.largeStraight;
+		this.fullHouse;
+		this.chance;
+		this.yatzy;
+	}
+}

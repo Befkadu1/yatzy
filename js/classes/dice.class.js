38b0967fb726d4af3fcc0708ee6c4a4645acbb63
Joel Nilsson <joel.o.nilsson@gmail.com>
2017-03-24 21:20:00 +0100

    Fixat så att tärningarna visas som unicode-symboler istället för siffror

--- js/classes/dice.class.js
+++ js/classes/dice.class.js
@@ -4,6 +4,37 @@ class Dice {
 		this.id = id;
 		this.value = value;
 		this.locked = locked;
+		this.setSymbol();
+	}
+
+	set val(value){
+		this.value = value;
+		this.setSymbol();
+	}
+
+	setSymbol(){
+		switch(this.value){
+			case 1:
+				this.symbol = '&#9856;';
+				break;
+			case 2:
+				this.symbol = '&#9857;';
+				break;
+			case 3:
+				this.symbol = '&#9858;';
+				break;
+			case 4:
+				this.symbol = '&#9859;';
+				break;
+			case 5:
+				this.symbol = '&#9860;';
+				break;
+			case 6:
+				this.symbol = '&#9861;';
+				break;
+			default:
+				this.symbol = '	&#127922;';
+		}
 	}
 
 	/*set value(value){

2ae0982188e2d6d8df392c05b7ac998b9bd98b37
johanjonsson <johan.jonsson95@hotmail.com>
2017-03-28 13:51:32 +0200

    fixade random funktion samt koopplat den till throw och låsa tärningar funkar!

--- js/classes/dice.class.js
+++ js/classes/dice.class.js
@@ -5,31 +5,48 @@ class Dice {
 		this.value = value;
 		this.locked = locked;
 		this.setSymbol();
+		this.setClass(this.locked);
 	}
 
 	set val(value){
 		this.value = value;
 		this.setSymbol();
+		
+	}
+
+	setClass(locked){
+		if(this.locked){
+           this.lockedClass = 'thisLocked';
+		}
+		else
+			this.lockedClass = '';
 	}
 
 	setSymbol(){
 		switch(this.value){
+
 			case 1:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9856;';
 				break;
 			case 2:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9857;';
 				break;
 			case 3:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9858;';
 				break;
 			case 4:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9859;';
 				break;
 			case 5:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9860;';
 				break;
 			case 6:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9861;';
 				break;
 			default:

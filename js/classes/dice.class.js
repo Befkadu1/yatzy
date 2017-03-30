cbb2140819a2bca7b19602c248b7a56dc1073664
Joel Nilsson <joel.o.nilsson@gmail.com>
2017-03-30 23:08:35 +0200

    Revert "Kommenterat ut console loggar och skrivit kommentarer på egen kod"
    This reverts commit f8dc67f504fae44131453528adc1c6603e839615.

--- js/classes/dice.class.js
+++ js/classes/dice.class.js
@@ -26,26 +26,39 @@ class Dice {
 		switch(this.value){
 
 			case 1:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9856;';
 				break;
 			case 2:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9857;';
 				break;
 			case 3:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9858;';
 				break;
 			case 4:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9859;';
 				break;
 			case 5:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9860;';
 				break;
 			case 6:
+			console.log("Jag är här!",this.value);
 				this.symbol = '&#9861;';
 				break;
 			default:
 				this.symbol = '	&#127922;';
 		}
 	}
+
+	/*set value(value){
+       this.value = value;
+	}
+	set locked(locking){
+		this.locked = locking;
+	}*/
        
 }
